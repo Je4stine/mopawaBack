e0280d11d913277d78ba79d13505f7f737659198
Amos Mutai <amosmutai04@gmail.com>
2023-02-09 11:29:34 +0300

    Fix on nodejs middleware not returning status

--- server.js
+++ server.js
@@ -20,52 +20,56 @@ app.get('/', (req, res)=>{
     res.send("<h1> App running</h1>");
 })
 
-const generateToken = async (req, res, next )=>{
+const generateToken = async (req,res)=>{
     const secret = "Hv6P9pXWXBpshSmz";
     const consumer = "5AAItrZI8QaNtgHDsuWl2AuufV81kfNJ";
 
     const auth = new Buffer.from(`${consumer}:${secret}`).toString("base64");
+    const{phone,amount}=req.body
 
-    await axios.get(
+    axios.get(
         "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials",{
             headers:{
                 authorization:`Basic ${auth}`
             }
         }
-    ).then((response)=>{
-        res.status(200);
-        token = response.data.access_token
-        console.log(response.data.access_token)
-      
+    ).then(async (response)=>{
+        console.log("Token: ",response.data.access_token)
+        let token = response.data.access_token
+        axios.post(
+            "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest",
+            {    
+              "BusinessShortCode":"174379",    
+              "Password": "MTc0Mzc5YmZiMjc5ZjlhYTliZGJjZjE1OGU5N2RkNzFhNDY3Y2QyZTBjODkzMDU5YjEwZjc4ZTZiNzJhZGExZWQyYzkxOTIwMTYwMjE2MTY1NjI3",    
+              "Timestamp":"20160216165627",    
+              "TransactionType": "CustomerPayBillOnline",    
+              "Amount":"1",    
+              "PartyA":"254748737342",    
+              "PartyB":"174379",    
+              "PhoneNumber":"254748737342",    
+              "CallBackURL":"https://0350-102-215-189-220.ngrok.io ",    
+              "AccountReference":"Test",    
+              "TransactionDesc":"Test"
+             },
+             {
+                headers:{
+                    Authorization:`Bearer ${token}`
+                }
+             },
+             
+        ).then((response)=>{
+            res.status(200).json(response)
+        }).catch(err=>{
+            let error = {}
+            if(err.response){
+                error.status=err.status||400
+                error.message=err.config.data||""
+                error.code=err.code||""
+                error.url=err.config.url||""
+            }
+            res.status(400).json(error)
+        })
     })
 }
 
-
-// unused 
-app.post ("/stk", generateToken, async (req, res)=>{
-    const phone = req.body.phone;
-    const amount = req.body.amount;
-
-    await axios.post(
-        "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest",
-        {    
-          "BusinessShortCode":"174379",    
-          "Password": "MTc0Mzc5YmZiMjc5ZjlhYTliZGJjZjE1OGU5N2RkNzFhNDY3Y2QyZTBjODkzMDU5YjEwZjc4ZTZiNzJhZGExZWQyYzkxOTIwMTYwMjE2MTY1NjI3",    
-          "Timestamp":"20160216165627",    
-          "TransactionType": "CustomerPayBillOnline",    
-          "Amount":"1",    
-          "PartyA":"254748737342",    
-          "PartyB":"174379",    
-          "PhoneNumber":"254748737342",    
-          "CallBackURL":"https://0350-102-215-189-220.ngrok.io ",    
-          "AccountReference":"Test",    
-          "TransactionDesc":"Test"
-         },
-         {
-            headers:{
-                Authorization:`Bearer${token}`
-            }
-         },
-         
-    )
-})
+app.post ("/stk", generateToken)
